24b10b6371d8af1e1947074f1650c3a8d4fc5a9b
Corinne Beard <corbeard@iu.edu>
2025-02-24 13:16:57 -0500

    created core webpages for final project

---
+++ week6/js/external.js
@@ -0,0 +1,25 @@
+console.log("Umm... where are we?");
+
+const birthYear = 2003;
+
+let myName = "Cornbread";
+
+console.log(myName, "was born", birthYear);
+
+myName = "Corinne Beard";
+
+// const variables can't change their values
+// birthYear = 2002;
+
+console.warn(myName, "may have devoured 10 souls")
+
+console.log("myName is", myName. length, "characters long");
+
+console.log("myName includes corn", myName.toUpperCase(). includes("CORN"));
+
+console.log("myName has uppercase:");
+
+myName = "Corinne";
+
+console.log("myName has uppercase:", myName !== myName.toLowerCase());
+console.log("myName has lowercase:", myName !== myName.toUpperCase());
