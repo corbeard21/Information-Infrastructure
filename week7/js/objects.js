f4b076ca40eabce208df868e03fddd0fea98182c
Corinne Beard <corbeard@iu.edu>
2025-02-25 11:35:48 -0500

    week 7 object practice

---
+++ week7/js/objects.js
@@ -0,0 +1,30 @@
+console.log ("Objects are us");
+
+const myObj = {};
+
+const person = {
+    // properties
+    name: "Cornbread",
+    age: 21,
+    eyeColor: "brown",
+    hairColor: "pink",
+    skinColor: "tan",
+    vision: "yes",
+    canDevourSouls: true,
+
+    // methods
+    sayHello () {
+        console.log ("Salutations my dearest enemy");
+        console.log (person.name);
+    }
+};
+
+console.log (person.name);
+
+person.sayHello ();
+
+function whatDoesTheFoxSay ( {
+    console.log ("Hatee-Hatee-Hatee-ho!");
+})
+
+whatDoesTheFoxSay();
