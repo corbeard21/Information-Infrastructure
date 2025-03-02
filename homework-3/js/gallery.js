9b8de0a803f2e2f862da06cf3cf8c5cad6144a9d
Corinne Beard <corbeard@iu.edu>
2025-03-01 19:28:08 -0500

    working on page 3

--- homework-3/js/gallery.js
+++ homework-3/js/gallery.js
@@ -17,22 +17,19 @@ console.log(itemsAltRef);
 
 console.log(titleRef.innerHTML);
 
-const ulRef = document.querySelector("body > ul");
+const ulRef = document.querySelector("body > section");
 
 console.log(ulRef.innerHTML);
 
 ulRef.innerHTML = "";
 
-const img = document.createElement("img");
-
-img.src = "https://fastly.picsum.photos/id/804/300/400.jpg?hmac=Y6cx9oentnw75-Q7gYdlaJBBAnSAlUHRYXbvFMIrZNI";
-
-img.alt = "First Image";
-
+const photos = [
+    "https://fastly.picsum.photos/id/804/300/400.jpg?hmac=Y6cx9oentnw75-Q7gYdlaJBBAnSAlUHRYXbvFMIrZNI", "https://fastly.picsum.photos/id/185/300/400.jpg?hmac=7jvc9SGhArOCJhgLoOfYSTezJsc1xrIXhDOXkiZSrbs", "https://fastly.picsum.photos/id/229/300/400.jpg?hmac=CZOWn8sQ8oa9EfbMZw5tYqPMNKK0mNOYUsEgz81K8io", "https://fastly.picsum.photos/id/211/300/400.jpg?hmac=l0zi1AkBxaPoAMnWav0iWH8TsWfblHXdm_IGSNDMFWQ"
+];
 
 for (let i = 0; i < photos.length; i++) {
     const photo = photos[i];
-    ulRef.innerHTML += "<li class='item' >" + photo + "</li>";
+        ulRef.innerHTML += "<section img class='photo' >" + photo + "</section>";
 
-    
+
 }
