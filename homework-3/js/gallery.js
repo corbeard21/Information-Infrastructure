8afab6ae728b11c40a3a3e6aa72be13ecd713146
Corinne Beard <corbeard@iu.edu>
2025-03-03 12:17:34 -0500

    finished page 3

--- homework-3/js/gallery.js
+++ homework-3/js/gallery.js
@@ -3,17 +3,9 @@ const titleRef = document.getElementById("gallery");
 
 console.log(titleRef);
 
-const itemsRef = document.getElementsByClassName("photo");
-
-console.log(itemsRef);
-
 const altTitleRef = document.querySelector("#gallery");
 
 console.log(altTitleRef);
-
-const itemsAltRef = document.querySelectorAll(".photo");
-
-console.log(itemsAltRef);
 
 console.log(titleRef.innerHTML);
 
@@ -23,13 +15,21 @@ console.log(ulRef.innerHTML);
 
 ulRef.innerHTML = "";
 
-const photos = [
-    "https://fastly.picsum.photos/id/804/300/400.jpg?hmac=Y6cx9oentnw75-Q7gYdlaJBBAnSAlUHRYXbvFMIrZNI", "https://fastly.picsum.photos/id/185/300/400.jpg?hmac=7jvc9SGhArOCJhgLoOfYSTezJsc1xrIXhDOXkiZSrbs", "https://fastly.picsum.photos/id/229/300/400.jpg?hmac=CZOWn8sQ8oa9EfbMZw5tYqPMNKK0mNOYUsEgz81K8io", "https://fastly.picsum.photos/id/211/300/400.jpg?hmac=l0zi1AkBxaPoAMnWav0iWH8TsWfblHXdm_IGSNDMFWQ"
-];
+const photos = ["https://fastly.picsum.photos/id/804/300/400.jpg?hmac=Y6cx9oentnw75-Q7gYdlaJBBAnSAlUHRYXbvFMIrZNI", "https://fastly.picsum.photos/id/185/300/400.jpg?hmac=7jvc9SGhArOCJhgLoOfYSTezJsc1xrIXhDOXkiZSrbs", "https://fastly.picsum.photos/id/229/300/400.jpg?hmac=CZOWn8sQ8oa9EfbMZw5tYqPMNKK0mNOYUsEgz81K8io", "https://fastly.picsum.photos/id/211/300/400.jpg?hmac=l0zi1AkBxaPoAMnWav0iWH8TsWfblHXdm_IGSNDMFWQ"]
+
+const gallerySection = document.getElementById("gallery");
 
 for (let i = 0; i < photos.length; i++) {
     const photo = photos[i];
-        ulRef.innerHTML += "<section img class='photo' >" + photo + "</section>";
 
+    const imgElement = document.createElement("img");
 
+    imgElement.src = photo;
+
+    imgElement.alt = "Image ${i + 1}";
+
+    imgElement.height = 300;
+
+    gallerySection.appendChild(imgElement);
+    
 }
