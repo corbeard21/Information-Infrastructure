653cbb0b207e8131c574a6881fe76d064504ece1
Corinne Beard <corbeard@iu.edu>
2025-03-06 11:50:23 -0500

    pokemon practice

--- week8/js/pokemon.js
+++ week8/js/pokemon.js
@@ -59,7 +59,28 @@ function startJourney() {
 
     newSection.innerHTML += "<h4>" + pokemon.name + "</h4>";
 
+    newSection.dataset.pokemonName = pokemon.name;
+    newSection.dataset.pokemonImage = pokemon.img;
+    newSection.onclick = choosePokemon;
+
+    newSection.onclick - choosePokemon;
+
     journeyRef.appendChild(newSection);
     console.log(pokemon);
+  }
+}
+
+function choosePokemon(e) {
+  console.log(e.currentTarget);
+  const pokemonName = e.currentTarget.dataset.pokemonName;
+  const pokemonImage = e.currentTarget.dataset.pokemonImage;
+
+  const confirmChoice = confirm("You chose " + pokemonName);
+
+  if (confirmChoice) {
+    journeyRef.innerHTML +=
+      "You chose " + pokemonName + " as your starter Pokemon.";
+
+    myPokemon = { name: pokemonName, img: pokemonImage };
   }
 }
