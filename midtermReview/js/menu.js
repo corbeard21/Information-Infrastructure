1d8d84f62a7f1c9109453f17fc2c722791d044d5
Corinne Beard <corbeard@iu.edu>
2025-03-11 11:57:42 -0400

    midterm practice

---
+++ midtermReview/js/menu.js
@@ -0,0 +1,27 @@
+// Reference to DOM
+
+const menuRef = document.querySelector("#menu");
+
+// Data
+
+const menuItems = [
+  {
+    image:
+      "https://s7d1.scene7.com/is/image/mcdonalds/DC_202106_0336_LargeVanillaCone_1564x1564-1:nutrition-calculator-tile",
+    name: "Vanilla Cone",
+  },
+  {
+    image:
+      "https://s7d1.scene7.com/is/image/mcdonalds/DC_201907_1509_MediumChocolateShake_Glass_A1_1564x1564-1:nutrition-calculator-tile",
+    name: "Medium Chocolate Shake",
+  },
+  {
+    image:
+      "https://s7d1.scene7.com/is/image/mcdonalds/DC_201907_1598_MediumVanillaShake_Glass_A1_1564x1564-1:nutrition-calculator-tile",
+    name: "Medium Vanilla Shake",
+  },
+];
+
+for (let i = 0; i < menuItems.length; i++) {
+  const menu = menuItems[i];
+}
