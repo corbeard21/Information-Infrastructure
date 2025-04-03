8e1dd331c6bf4bef4facd347aaffa553cab0251f
Corinne Beard <corbeard@iu.edu>
2025-04-03 10:43:42 -0400

    week 12 practice

--- week12/js/app.js
+++ week12/js/app.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
 {
   /* // TODO: Add SDKs for Firebase products that you want to use */
@@ -20,5 +21,5 @@ const firebaseConfig = {
 };
 
 //   Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
--- week12/js/home.js
+++ week12/js/home.js
@@ -1,2 +1,28 @@
-import { secret } from "./app.js";
-console.log("home", secret);
+import { app } from "./app.js";
+console.clear();
+import {
+  getFirestore,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+
+const db = getFirestore(app);
+
+const hadCollection = collection(db, "hopesanddreams");
+
+const dreamsRef = document.querySelector("#dreams");
+
+async function getHad() {
+  const hadDocs = await getDocs(hadCollection);
+  console.log("Hungry hippos");
+  dreamsRef.innerHTML = "";
+  for (let i = 0; i < hadDocs.docs.length; i++) {
+    const currentDream = hadDocs.docs[i];
+    console.log(currentDream.id, currentDream.data());
+
+    const dreamData = currentDream.data();
+    dreamsRef.innerHTML += "<li>" + dreamData.text + "</li>";
+  }
+}
+
+getHad();
